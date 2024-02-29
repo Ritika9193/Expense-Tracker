425827dc63d9148046de69e9b445354dedc78bd8
Ritika9193 <118365693+Ritika9193@users.noreply.github.com>
2024-02-29 15:06:00 +0000

    expense tracker

--- 01expensetracker/src/components/AddTransaction.jsx
+++ 01expensetracker/src/components/AddTransaction.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
+import { useTransaction } from "../context/Transactioncontext";
 
 export const Addtransaction=()=>{
+    const [text,setTransaction]=useState('')
+    const [amount,setAmount]=useState('')
+
+    const {addExpense}=useTransaction()
+
+    const onsubmit=(e)=>{
+        e.preventDefault();
+        if(text.trim()=='' || amount.trim()==''){
+            return;
+        }
+        const transactionamount=+amount
+        addExpense(text,transactionamount);
+        setTransaction('')
+        setAmount('')
+    }
+
     return(
         <>
             <h3>Add new transaction</h3>
             <form >
                 <div className="form-control">
                 <label htmlFor="text">Text</label>
-                <input type="text"  placeholder="Enter text..." />
+                <input type="text" 
+                value={text}
+                onChange={(e)=>setTransaction(e.target.value)}
+                placeholder="Enter text..." />
                 </div>
                 <div className="form-control">
                 <label htmlFor="amount">
                     Amount <br />
                     (negative - expense, positive - income)
                 </label>
-                <input type="number"  placeholder="Enter amount..." />
+                <input type="number" 
+                value={amount}
+                onChange={(e)=>setAmount(e.target.value)}
+                placeholder="Enter amount..." />
                 </div>
-                <button className="btn">Add transaction</button>
+                <button className="btn" onClick={onsubmit}>Add transaction</button>
             </form>
         </>
     )
--- 01expensetracker/src/components/TransactionList.jsx
+++ 01expensetracker/src/components/TransactionList.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useTransaction } from "../context/Transactioncontext";
+
 
 export const TransactionList=()=>{
+    const {transaction,deleteExpense}=useTransaction()
     return(
         <>
             <h3>History</h3>
             <ul  className="list">
-            <li className="minus">
-            Cash <span>-$400</span><button className="delete-btn">x</button>
-            </li> 
+            {transaction.map((transaction)=>(
+                <li key={transaction.id}className={transaction.amount<0?'minus':'plus'}>
+                {transaction.text} <span>{transaction.amount<0?'-':'+'}${Math.abs(transaction.amount)}</span>
+                <button className="delete-btn"
+                onClick={()=>deleteExpense(transaction.id)}
+                >x</button>
+            </li>
+            ))} 
       </ul>
         </>
     )
